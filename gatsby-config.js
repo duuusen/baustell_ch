1f2eb6a5fb9d124bbf8bcf8c4398a798b25b4aca
duuusen <halloduy@gmail.com>
2021-10-11 18:38:56 +0200

    added missing icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `images/b-icon.png`
+        icon: `src/images/b-icon.png`
       },
     },
     {
