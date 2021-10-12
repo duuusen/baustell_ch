fd303180e274efb438c70c960b6aec1f2ba75218
duuusen <halloduy@gmail.com>
2021-10-12 13:28:15 +0200

    changed bg color (safari/mobile) to baustell red

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#ffffff`,
+        background_color: `#ED1C24`,
+        theme_color: `#ED1C24`,
         display: `minimal-ui`,
         icon: `src/images/b-icon.png`
       },
