5177f7b9d4916f7b28a3e114359e5f3b0599b6b0
Fabian Frey <50176941+fabidesign@users.noreply.github.com>
2022-06-29 22:30:33 +0200

    initial

---
+++ src/components/main.js
@@ -0,0 +1,65 @@
+import * as React from "react"
+
+import "./global.css"
+import "./fonts.css"
+import "./baugeruest.css"
+
+const Main = () => {
+  return (
+      <div class="main-grid">
+		<div id="vertical-eins" class="metal-vertical"></div>
+		<div id="testy">
+			<div style={{height: '100px'}}></div>
+			<div class="metal-horizontal"></div>
+			<div style={{height: '100px'}}></div>
+			<div class="wood-horizontal-yellow">
+				<div class="plate-left"></div>
+				<div class="plate-right"></div>
+			</div>
+		</div>
+		<div id="vertical-zwei" class="metal-vertical"></div>
+		<div>
+			<div style={{height: '100px'}}></div>
+			<div class="metal-horizontal"></div>
+			<div style={{height: '100px'}}></div>
+			<div class="wood-horizontal-yellow">
+				<div class="plate-left"></div>
+				<div class="plate-right"></div>
+			</div>
+		</div>
+		<div id="vertical-drei" class="metal-vertical"></div>
+		<div>
+			<div style={{height: '100px'}}></div>
+			<div class="metal-horizontal"></div>
+			<div style={{height: '100px'}}></div>
+			<div class="wood-horizontal-yellow">
+				<div class="plate-left"></div>
+				<div class="plate-right"></div>
+			</div>
+		</div>
+		<div id="vertical-vier" class="metal-vertical"></div>
+		<div>
+			<div style={{height: '100px'}}></div>
+			<div class="metal-horizontal"></div>
+			<div style={{height: '100px'}}></div>
+			<div class="wood-horizontal-yellow">
+				<div class="plate-left"></div>
+				<div class="plate-right"></div>
+			</div>
+		</div>
+		<div id="vertical-funf" class="metal-vertical"></div>
+		<div>
+			<div style={{height: '100px'}}></div>
+			<div class="metal-horizontal"></div>
+			<div style={{height: '100px'}}></div>
+			<div class="wood-horizontal-yellow">
+				<div class="plate-left"></div>
+				<div class="plate-right"></div>
+			</div>
+		</div>
+		<div id="vertical-sechs" class="metal-vertical"></div>
+	</div>
+  )
+}
+
+export default Main
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,13 @@
 import * as React from "react"
-import { graphql } from "gatsby"
-import Fade from 'react-reveal/Fade';
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import Instagram from "../components/instagram"
+import Main from "../components/main"
 
 const IndexPage = ({ data }) => {
-  const profiles = data.allMarkdownRemark.nodes
-  console.log(data)
 
   return (
-    <Layout>
-      <Instagram></Instagram>
-      <Seo title="Home" />
-      <section className="wrap">
-      <Fade bottom distance={'10px'}>
-        {profiles.map(profile => (
-            <div className={`tile ${profile.frontmatter.style}`} key={profile.id}>
-              <h2>{profile.frontmatter.name}</h2>
-              <h3>{profile.frontmatter.description}</h3>
-              <div dangerouslySetInnerHTML={{ __html: profile.html }} />
-                <a className="website" href={profile.frontmatter.url} target="_blank">Website →<br></br></a>
-                <a className="contact" href={`mailto:${profile.frontmatter.email}`}>Contact →</a>
-            </div>
-        ))}
-        </Fade>
-      </section>
-    </Layout>
+    <Main></Main>
   );
 
 }
 
 export default IndexPage
-
-export const query = graphql`
-  {
-    allMarkdownRemark(sort: {fields: frontmatter___position, order: ASC}) {
-      nodes {
-        frontmatter {
-          description
-          email
-          url
-          name
-          position
-          style
-        }
-        html
-        id
-      }
-    }
-  }
-`
