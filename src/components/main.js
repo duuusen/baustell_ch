a6da7b7a7e5c0b0c2ce2ad45b533b564a241ec16
Fabian Frey <50176941+fabidesign@users.noreply.github.com>
2022-06-30 21:53:30 +0200

    hallo

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 import "./global.css"
 import "./fonts.css"
@@ -9,7 +10,7 @@ const Main = () => {
       <div class="main-grid">
 		<div id="vertical-eins" class="metal-vertical"></div>
 		<div id="testy">
-			<div style={{height: '100px'}}></div>
+			<div style={{height: '150px'}}></div>
 			<div class="metal-horizontal"></div>
 			<div style={{height: '100px'}}></div>
 			<div class="wood-horizontal-yellow">
@@ -19,7 +20,12 @@ const Main = () => {
 		</div>
 		<div id="vertical-zwei" class="metal-vertical"></div>
 		<div>
-			<div style={{height: '100px'}}></div>
+			<div style={{height: '15px'}}></div>
+      <div class="baustell-logo">
+        <img src="../images/Logo Transparent Black.svg">
+        </img>
+      </div>
+      <div style={{height: '15px'}}></div>
 			<div class="metal-horizontal"></div>
 			<div style={{height: '100px'}}></div>
 			<div class="wood-horizontal-yellow">
@@ -29,7 +35,7 @@ const Main = () => {
 		</div>
 		<div id="vertical-drei" class="metal-vertical"></div>
 		<div>
-			<div style={{height: '100px'}}></div>
+			<div style={{height: '150px'}}></div>
 			<div class="metal-horizontal"></div>
 			<div style={{height: '100px'}}></div>
 			<div class="wood-horizontal-yellow">
@@ -39,7 +45,7 @@ const Main = () => {
 		</div>
 		<div id="vertical-vier" class="metal-vertical"></div>
 		<div>
-			<div style={{height: '100px'}}></div>
+			<div style={{height: '150px'}}></div>
 			<div class="metal-horizontal"></div>
 			<div style={{height: '100px'}}></div>
 			<div class="wood-horizontal-yellow">
@@ -49,7 +55,7 @@ const Main = () => {
 		</div>
 		<div id="vertical-funf" class="metal-vertical"></div>
 		<div>
-			<div style={{height: '100px'}}></div>
+			<div style={{height: '150px'}}></div>
 			<div class="metal-horizontal"></div>
 			<div style={{height: '100px'}}></div>
 			<div class="wood-horizontal-yellow">
